4a8323cd94a32f29bd84089743c6e1f03ae47b3a
Benjamin Heath <benjamin.joel.heath@gmail.com>
2013-09-29 01:24:48 -0700

    Initial commit

---
+++ examples/example1.c
@@ -0,0 +1,48 @@
+//this example code shows how to put some text in nametable
+//it assumes that you have ASCII-encoded font in the CHR tiles $00-$3f
+
+#include "neslib.h"
+
+
+//this macro is used remove need of calculation of the nametable address in runtime
+
+#define NTADR(x,y) ((0x2000|((y)<<5)|x))
+
+
+//put a string into the nametable
+
+void put_str(unsigned int adr,const char *str)
+{
+	vram_adr(adr);
+
+	while(1)
+	{
+		if(!*str) break;
+		vram_put((*str++)-0x20);//-0x20 because ASCII code 0x20 is placed in tile 0 of the CHR
+	}
+}
+
+
+
+void main(void)
+{
+	//rendering is disabled at the startup, and palette is all black
+
+	pal_col(1,0x03);//set while color
+
+	//you can't put data into vram through vram_put while rendering is enabled
+	//so you have to disable rendering to put things like text or a level map
+	//into the nametable
+
+	//there is a way to update small number of nametable tiles while rendering
+	//is enabled, using set_vram_update and an update list
+
+	put_str(NTADR(0,2),"HELLO, KEN BREWER!");
+	put_str(NTADR(2,3),"I BET YOU DIDN'T EXPECT THIS.");
+	put_str(NTADR(2,5),"BUT HERE IT IS!");
+	put_str(NTADR(2,6),"    HELLO NES!");
+
+	ppu_on_all();//enable rendering
+
+	while(1);//do nothing, infinite loop
+}
